214c125d93f73750c02608a83cbe13b052cf8e40
igara4 <fifty.storm.snowman@gmail.com>
2024-12-16 12:45:40 +0900

    Vercelでのデプロイ練習用に作成していたカウントアプリをコミット

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Counter from "./components/Counter";
+
+const App=()=>{
+    return(
+        <>
+            <div className="ui container" style={{marginTop:'20px'}}>
+                <Counter/>
+            </div>
+        </>
+        
+    );
+};
+
+export default App;
